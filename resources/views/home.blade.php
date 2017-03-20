c5c3bf3281166084798d84975096a61b94745d69
Reaper45 <jomwashighadi@gmail.com>
2017-03-20 13:43:56 +0300

    Done with authentication

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container" style="width: 100%;">
+    <div class="row">
+        <div class="col-md-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Menu</div>
+
+                <div class="panel-body">
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Stats</div>
+
+                <div class="panel-body">
+                    Also Coming Soon!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/includes/master.blade.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: reaper45
+ * Date: 2/28/17
+ * Time: 4:34 PM
+ */
+?>
