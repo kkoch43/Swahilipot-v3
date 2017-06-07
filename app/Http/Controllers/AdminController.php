a79faa98624396aa6795e453cd73b94531c04d02
Reaper45 <jomwashighadi@gmail.com>
2017-06-07 23:27:37 +0300

    Member+Admin extend usercontroller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AdminController extends Controller
+class AdminController extends UserController
 {
   /**
    * Create a new controller instance.
